8d3cd887b6ae3695d667f092c3f6d5e0f40fa89b
BeatriceOana <timisbeatriceoana@gmail.com>
2024-02-29 19:22:36 +0200

    Un al doilea folder.

---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class test {
+      public static void main(String[] args) {
+          int a = 5;
+          int b= 3;
+        System.out.println("Suma lui dintre a si b este " + (a+b) + ".");
+    }
+
+}
